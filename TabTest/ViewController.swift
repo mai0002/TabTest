c06187c802d63b0ab8dc2994587a4220eeb7e7e1
MaiInagaki <navy.blue.02mi@gmail.com>
2018-11-05 14:07:28 +0900

    Initial Commit

---
+++ TabTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabTest
+//
+//  Created by 稲垣麻衣 on 2018/11/05.
+//  Copyright © 2018年 稲垣麻衣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
